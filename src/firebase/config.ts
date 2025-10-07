baa3aa2cba0bbdf2466aab88a8173681394639c2
SA RA <sarsor6578@gmail.com>
2025-10-07 10:25:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1747612531-4566b",
+  "appId": "1:904250354189:web:2ee6ff2af93fb21c100344",
+  "apiKey": "AIzaSyB7aylOS5nxZPr5_jnPKvI79NLXxBkKOw8",
+  "authDomain": "studio-1747612531-4566b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "904250354189"
+};
